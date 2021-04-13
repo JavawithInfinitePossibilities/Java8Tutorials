17b20e173989ed80ee3f07bad3089c07335114e1
Lenovo <Lenovo@DESKTOP-8HSI2JK>
2021-04-13 13:56:15 +0530

    This is a class defination declaration

---
+++ Java8Tutorial/src/main/java/com/sid/java8/tutorials/Chapter15/MultipleClassDeclaration.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.sid.java8.tutorials.Chapter15;
+
+/**
+ * @author Lenovo Class can contain multiple class inside and can be declared a
+ *         private and protected. <br/>
+ *         But if a class is declared outside the class and inside the same file
+ *         then class property are going to use like
+ *         1.	A class can't declare as private or protected.
+ *         2.	A class can only declared as default.
+ *         3.	A class can be declared as Final or Abstract. Not both.
+ */
+
+public class MultipleClassDeclaration {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private class Myclass2 {
+
+	}
+}
