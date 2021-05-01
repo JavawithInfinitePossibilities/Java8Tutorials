c29aa0be563636b1f9df46987d756bc6b31dc0fd
Lenovo <Lenovo@DESKTOP-8HSI2JK>
2021-05-01 23:38:52 +0530

    using semaphore without runnable class and in Java-8 lambda expression.

--- Java8Tutorial/src/main/java/com/sid/java8/tutorials/Chapter16JavaAdvancedMultithreadingThreadTutorial/JavaThreadSemaphoreExampleRestaurentCustomer.java
+++ Java8Tutorial/src/main/java/com/sid/java8/tutorials/Chapter16JavaAdvancedMultithreadingThreadTutorial/JavaThreadSemaphoreExampleRestaurentCustomer.java
@@ -5,6 +5,7 @@ package com.sid.java8.tutorials.Chapter16JavaAdvancedMultithreadingThreadTutoria
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
@@ -21,6 +22,7 @@ public class JavaThreadSemaphoreExampleRestaurentCustomer {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+
 		Restaurent restaurent = new Restaurent(3);
 		ExecutorService executorService = Executors.newFixedThreadPool(5);
 		IntStream.range(0, 10).forEach((i) -> {
@@ -32,6 +34,33 @@ public class JavaThreadSemaphoreExampleRestaurentCustomer {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+
+		Semaphore semaphore = new Semaphore(4);
+		ExecutorService executorService2 = Executors.newFixedThreadPool(10);
+		IntStream.range(0, 10).forEach((i) -> {
+			executorService2.execute(getRunnable(semaphore));
+		});
+		executorService2.shutdown();
+		try {
+			executorService2.awaitTermination(1, TimeUnit.HOURS);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static Runnable getRunnable(Semaphore semaphore) {
+		return () -> {
+			try {
+				semaphore.acquire();
+				System.out.println("Currently " + Thread.currentThread().getName() + " acquired the lock.");
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} finally {
+				System.out.println("Currently " + Thread.currentThread().getName() + " released the lock.");
+				semaphore.release();
+			}
+		};
 	}
 
 }
