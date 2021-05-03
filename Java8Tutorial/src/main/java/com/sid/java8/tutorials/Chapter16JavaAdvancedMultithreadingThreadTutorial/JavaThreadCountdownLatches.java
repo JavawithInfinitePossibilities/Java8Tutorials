ad5485999fed54c8a0d9d983f1d33bc06d6a6f23
siddhant <javawithinfinitepossibilities@gmail.com>
2021-05-03 16:53:20 +0530

    Created a branch to stimulate merge

--- Java8Tutorial/src/main/java/com/sid/java8/tutorials/Chapter16JavaAdvancedMultithreadingThreadTutorial/JavaThreadCountdownLatches.java
+++ Java8Tutorial/src/main/java/com/sid/java8/tutorials/Chapter16JavaAdvancedMultithreadingThreadTutorial/JavaThreadCountdownLatches.java
@@ -22,29 +22,34 @@ public class JavaThreadCountdownLatches {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		/*
-		 * CountDownLatch countDownLatch = new CountDownLatch(5); ExecutorService
-		 * executorService = Executors.newFixedThreadPool(5); for (int i = 0; i < 5;
-		 * i++) { executorService.execute(new JavaCountdownLatchWorker(countDownLatch,
-		 * i)); }
-		 *//**
-			 * executorService.shutdown(); If we use before the below line, <br/>
-			 * then we are getting exception: <br/>
-			 * java.util.concurrent.RejectedExecutionException: Task
-			 * java.util.concurrent.FutureTask@19469ea2
-			 *//*
-				 * executorService.execute(new JavaCountdownLatchMger(countDownLatch, 5));
-				 * executorService.shutdown(); System.out.println("This is main thread...");
-				 * 
-				 * try { countDownLatch.await(); executorService.awaitTermination(1,
-				 * TimeUnit.HOURS); } catch (InterruptedException e) { e.printStackTrace(); }
-				 * System.out.println("Main thread execution complete...");
-				 */
+
+		CountDownLatch countDownLatch = new CountDownLatch(5);
+		ExecutorService executorService = Executors.newFixedThreadPool(5);
+		for (int i = 0; i < 5; i++) {
+			executorService.execute(new JavaCountdownLatchWorker(countDownLatch, i));
+		}
+		/**
+		 * executorService.shutdown(); If we use before the below line, <br/>
+		 * then we are getting exception: <br/>
+		 * java.util.concurrent.RejectedExecutionException: Task
+		 * java.util.concurrent.FutureTask@19469ea2
+		 */
+		executorService.execute(new JavaCountdownLatchMger(countDownLatch, 5));
+		executorService.shutdown();
+		System.out.println("This is main thread...");
+
+		try {
+			countDownLatch.await();
+			executorService.awaitTermination(1, TimeUnit.HOURS);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Main thread execution complete...");
 
 		/** Using Java-8 lambda expression **/
 		CountDownLatch latch = new CountDownLatch(10);
 		ExecutorService Service = Executors.newFixedThreadPool(5);
-		IntStream.range(0,10).forEach((i) -> {
+		IntStream.range(0, 10).forEach((i) -> {
 			Service.execute(() -> {
 				System.out.println("This is countdown latch worker thread id :" + i);
 				try {
