580c9b2899c2b6a5fc6b760642ea8886a8dc2516
siddhant <javawithinfinitepossibilities@gmail.com>
2021-07-16 16:46:37 +0530

    Change in the comment

--- Java8Tutorial/src/main/java/com/sid/java8/tutorials/Chapter15/MultipleClassDeclaration.java
+++ Java8Tutorial/src/main/java/com/sid/java8/tutorials/Chapter15/MultipleClassDeclaration.java
@@ -7,7 +7,7 @@ package com.sid.java8.tutorials.Chapter15;
  * @author Lenovo Class can contain multiple class inside and can be declared a
  *         private and protected. <br/>
  *         But if a class is declared outside the class and inside the same file
- *         then class property are going to use like
+ *         then class property are going to be like
  *         1.	A class can't declare as private or protected.
  *         2.	A class can only declared as default.
  *         3.	A class can be declared as Final or Abstract. Not both.
