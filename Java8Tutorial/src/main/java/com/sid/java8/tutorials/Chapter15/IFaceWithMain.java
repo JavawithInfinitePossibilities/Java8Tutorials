fcee4dca13e871cb86c3d3891697c8c6d6c590b7
siddhant <javawithinfinitepossibilities@gmail.com>
2021-07-16 13:40:28 +0530

    Interface with main method

---
+++ Java8Tutorial/src/main/java/com/sid/java8/tutorials/Chapter15/IFaceWithMain.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.sid.java8.tutorials.Chapter15;
+
+/**
+ * @author Lenovo
+ *
+ */
+public interface IFaceWithMain {
+	public static void main(String[] args) {
+		System.out.println("main method");
+	}
+}
